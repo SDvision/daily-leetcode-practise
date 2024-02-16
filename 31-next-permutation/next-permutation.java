4d88dcbbc427cfd9a99953490b08e9375c0a8c34
Sanchit Dahatonde <sanchit.dahatonde@gmail.com>
2024-02-17 04:15:44 +0530

    Time: 1 ms (27.12%) | Memory: 42.9 MB (58.58%) - LeetSync

--- 31-next-permutation/next-permutation.java
+++ 31-next-permutation/next-permutation.java
@@ -4,9 +4,16 @@ class Solution {
         if (nums.length == 1) {
             return;
         }
+        int minIdx = 0;
         for (int i = nums.length - 1; i > 0; i--) {
             if (nums[i] > nums[i - 1]) {
                 rPoint = i - 1;
+                for (int j = rPoint; j < nums.length; j++) {
+                    int minDiff = 0;  
+                    if (nums[rPoint] - nums[j] < minDiff) {
+                        minIdx = j;
+                    }
+                }
                 break;
             }
         }
@@ -14,14 +21,8 @@ class Solution {
             Arrays.sort(nums);
             return;
         }
-        int minIdx = 0;
-        for (int i = rPoint; i < nums.length; i++) {
-            int minDiff = 0;
-            
-            if (nums[rPoint] - nums[i] < minDiff) {
-                minIdx = i;
-            }
-        }
+        
+        
         int temp = nums[minIdx];
         nums[minIdx] = nums[rPoint];
         nums[rPoint] = temp;
