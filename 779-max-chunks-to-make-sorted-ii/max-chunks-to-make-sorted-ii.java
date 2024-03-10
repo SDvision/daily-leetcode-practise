28a1c37fc6a7e459b76f3f9399fdcd985c43f5c8
Sanchit Dahatonde <sanchit.dahatonde@gmail.com>
2024-03-10 13:34:28 +0530

    Time: 0 ms (100.00%) | Memory: 44.3 MB (59.97%) - LeetSync

--- 779-max-chunks-to-make-sorted-ii/max-chunks-to-make-sorted-ii.java
+++ 779-max-chunks-to-make-sorted-ii/max-chunks-to-make-sorted-ii.java
@@ -7,8 +7,14 @@ class Solution {
         int[] leftMax = new int[arr.length];
         int[] rightMin = new int[arr.length];
 
-        createLeftMax(arr, leftMax);
-        createRightMin(arr, rightMin);
+        leftMax[0] = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            leftMax[i] = Math.max(arr[i], leftMax[i - 1]);
+        }
+        rightMin[arr.length - 1] = arr[arr.length - 1];
+        for (int i = arr.length - 2; i >= 0; i--) {
+            rightMin[i] = Math.min(arr[i], rightMin[i + 1]);
+        }
 
         int ans = 0;
         for (int i = 0; i < arr.length - 1; i++) {
@@ -19,22 +25,4 @@ class Solution {
         return ans + 1;
     }
 
-    void createLeftMax(int[] arr, int[] leftMax) {
-        int max = arr[0];
-        leftMax[0] = arr[0];
-        for (int i = 1; i < arr.length; i++) {
-            max = Math.max(leftMax[i-1], arr[i]);
-            leftMax[i] = max;
-        }
-    }
-
-    void createRightMin(int[] arr, int[] rightMin) {
-        int length = arr.length;
-        int min = arr[length - 1];
-        rightMin[length - 1] = arr[length - 1];
-        for (int i = length - 2; i >= 0; i--) {
-            min = Math.min(rightMin[i+1], arr[i]);
-            rightMin[i] = min;
-        }
-    }
-}
+ }
