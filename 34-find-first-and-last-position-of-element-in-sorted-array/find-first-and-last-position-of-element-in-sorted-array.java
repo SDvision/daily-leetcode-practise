932e24ecd5caa0aaf2f0f131454aed502ecf4f52
Sanchit Dahatonde <sanchit.dahatonde@gmail.com>
2024-03-11 13:12:25 +0530

    Time: 0 ms (100.00%) | Memory: 45.6 MB (90.69%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,43 +1,37 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int s = 0;
-        int e = nums.length - 1;
-        
-        int[] result = new int[2];
-        result[0] = findStartIdx(nums, s, e, target);
-        result[1] = findEndIdx(nums, s, e, target);
-        return result;
+        int first = -1;
+        int last = -1;
+
+        first = findFirstOrLast(nums, target, true);
+        last = findFirstOrLast(nums, target, false);
+
+        return new int[]{first, last};
     }
 
-    int findStartIdx(int[] arr, int s, int e, int target) {
-        int index = -1;
-        while (s <= e) {
-            int mid = s + (e - s)/2;
-            if (target == arr[mid]) {
-                index = mid;
-                e = mid - 1;
-            } else if (target < arr[mid]) {
-                e = mid - 1;
+    int findFirstOrLast(int[] arr, int target, boolean isFirst) {
+        int l = 0;
+        int r = arr.length - 1;
+        int ans = -1;
+        while (l <= r) {
+            int m = l + (r - l)/2;
+            
+            if (arr[m] == target) {
+                ans = m;
+                if (isFirst) {
+                    r = m - 1;
+                } else {
+                    l = m + 1;
+                }
+            } else if (arr[m] < target) {
+                l = m + 1;
             } else {
-                s = mid + 1;
+                r = m - 1;
             }
         }
-        return index;
-    }
-    
-    int findEndIdx(int[] arr, int s, int e, int target) {
-        int index = -1;
-        while(s <= e) {
-            int mid = s + (e - s)/2;
-            if (target == arr[mid]) {
-                index = mid;
-                s = mid + 1;
-            } else if (target < arr[mid]) {
-                e = mid - 1;
-            } else {
-                s = mid + 1;
-            }
+        if (ans == -1) {
+            return -1;
         }
-        return index;
+        return ans;
     }
 }
