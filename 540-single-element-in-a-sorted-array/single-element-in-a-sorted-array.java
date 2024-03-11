3305b7f20a5fd598172e82bfef7ece0ca57cc0c7
Sanchit Dahatonde <sanchit.dahatonde@gmail.com>
2024-03-11 14:58:46 +0530

    Time: 0 ms (100.00%) | Memory: 50.1 MB (78.29%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
@@ -4,18 +4,14 @@ class Solution {
 
         int l = 0;
         int r = nums.length - 1;
-        int m = -1;
         while (l <= r) {
-            m = l + (r - l) / 2;
+            int m = l + (r - l) / 2;
             if (m != nums.length - 1 && nums[m] == nums[m ^ 1]) {
                 l = m + 1;
             } else {
-                if (m != 0 && m != nums.length - 1 && nums[m] != nums[m - 1] && nums[m] != nums[m + 1]) {
-                    return nums[m];
-                }
                 r = m - 1;
             }
         }
-        return nums[m];
+        return nums[l];
     }
 }
