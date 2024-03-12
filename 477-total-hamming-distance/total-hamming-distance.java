c91d0a61c358827b8d4debc76364103b334a251a
Sanchit Dahatonde <sanchit.dahatonde@gmail.com>
2024-03-12 11:41:14 +0530

    Time: 15 ms (69.19%) | Memory: 45.5 MB (5.39%) - LeetSync

--- 477-total-hamming-distance/total-hamming-distance.java
+++ 477-total-hamming-distance/total-hamming-distance.java
@@ -1,29 +1,15 @@
 class Solution {
     public int totalHammingDistance(int[] nums) {
         int thd = 0;
-        if (nums.length == 1) {
-            return thd;
-        }
-        for (int i = 0; i < nums.length - 1; i++) {
-            for (int j = i + 1; j < nums.length; j++) 
-                // thd += calHammingDist(nums[i], nums[j]);
-                thd += Integer.bitCount(nums[i] ^ nums[j]);
+        for (int i = 0; i < 32; i++) {
+            int oneBits = 0;
+            for (int j = 0; j < nums.length; j++) {
+                int num = nums[j];
+                if (((num >>= i) & 1) == 1)
+                    oneBits++;
+            }
+            thd += (oneBits) * (nums.length - oneBits);
         }
         return thd;
-    }
-    int calHammingDist(int num1, int num2) {
-        int xor = num1 ^ num2;
-        if (xor == 0) {
-            return 0;
-        }
-        int setBits = 0;
-        while (xor != 0) {
-            int bm = 1;
-            if ((xor & 1) != 0) {
-                setBits++;
-            }
-            xor >>= 1;
-        }
-        return setBits;
     }
 }
