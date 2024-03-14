07a4e0fd7e06fdf241d4c494aebf6688ae32f41d
Sanchit Dahatonde <sanchit.dahatonde@gmail.com>
2024-03-14 23:38:04 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (31.01%) - LeetSync

--- 162-find-peak-element/find-peak-element.java
+++ 162-find-peak-element/find-peak-element.java
@@ -3,18 +3,12 @@ class Solution {
         int l = 0;
         int r = nums.length - 1;
 
-        while (l <= r) {
-            int m = l + (r - l) / 2;
-            if (m - 1 < 0 || nums[m] > nums[m - 1]) {
-                if (m + 1 >= nums.length ||  nums[m] > nums[m + 1]) {
-                    return m;
-                } else {
-                    l = m + 1;
-                }
-            } else {
-                r = m - 1;
-            }
+        while (l < r) {
+            int m = (l + r) / 2;
+            if (nums[m] < nums[m + 1]) l = m + 1;
+            else r = m;
         }
-        return -1;
+
+        return l;
     }
 }
